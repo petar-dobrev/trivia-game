bab498240b50d9b66f0ea7e27c8c20dee35b45e0
petar-dobrev <92185890+petar-dobrev@users.noreply.github.com>
2022-04-21 13:40:50 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Leaderboard from "./pages/leaderboard/Leaderboard";
+import MainMenu from "./pages/main-menu/MainMenu";
+import Play from "./pages/play/Play";
+import Profile from "./pages/profile/Profile";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainMenu />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/users/:userId" element={<Profile />} />
+          <Route path="/play" element={<Play />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
