2f22a9bb96130ce7a66bc7b8a2735291e25bac7b
petar-dobrev <92185890+petar-dobrev@users.noreply.github.com>
2022-04-21 14:02:08 +0300

    Add main menu

--- src/pages/main-menu/MainMenu.jsx
+++ src/pages/main-menu/MainMenu.jsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import "./mainMenu.css";
+
 function MainMenu() {
-  return <div>MainMenu</div>;
+  const loggedIn = true; // Placeholder until state contex is added
+
+  const buttons = loggedIn ? (
+    <>
+      <Link to="/play">
+        <Button className="mainMenuButton" variant="contained" size="large" fullWidth={true}>
+          Play Game
+        </Button>
+      </Link>
+      <Link to="/users/:userId">
+        <Button className="mainMenuButton" variant="contained" size="large" fullWidth={true}>
+          View Profile
+        </Button>
+      </Link>
+      <Link to="/leaderboard">
+        <Button className="mainMenuButton" variant="contained" size="large" fullWidth={true}>
+          View Leaderboard
+        </Button>
+      </Link>
+      <Button className="mainMenuButton" variant="contained" size="large" fullWidth={true}>
+        Log out
+      </Button>
+    </>
+  ) : (
+    <>
+      <Button className="mainMenuButton" variant="contained" size="large" fullWidth={true}>
+        Log in / Register
+      </Button>
+      <Link to="/leaderboard">
+        <Button className="mainMenuButton" variant="contained" size="large" fullWidth={true}>
+          View Leaderboard
+        </Button>
+      </Link>
+    </>
+  );
+
+  return (
+    <div className="mainMenuContainer">
+      <Container maxWidth="lg">{buttons}</Container>
+    </div>
+  );
 }
 
 export default MainMenu;
